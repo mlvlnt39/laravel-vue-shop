731031ad431a2d5c714a07d22bac9e1cb5d3b8c5
Andrew Buikly <nulaehow@gmail.com>
2023-07-19 11:53:36 +0300

    Category routes and controllers

---
+++ app/Http/Controllers/Category/StoreController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
---
+++ app/Http/Controllers/Category/UpdateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdateController extends Controller
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
